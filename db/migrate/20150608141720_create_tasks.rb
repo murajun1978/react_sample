338897422e4c2bf66a25731a7e0fb35aa57550b1
murajun1978 <crazy.rendezvous.m.jun@gmail.com>
2015-06-08 23:34:19 +0900

    create tables

---
+++ db/migrate/20150608141720_create_tasks.rb
@@ -0,0 +1,10 @@
+class CreateTasks < ActiveRecord::Migration
+  def change
+    create_table :tasks do |t|
+      t.text :description
+      t.references :project, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
